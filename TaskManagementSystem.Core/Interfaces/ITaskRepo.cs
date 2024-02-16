b816880befdc70d9998e1b3a6dd020d9cb75caf9
abdelaziz tabbosha <abdelaziz.tabosha@nt-me.com>
2024-02-16 19:34:00 +0200

    Create Task Entity, Task  Repo Class, Task Repo interface Add the Basic Functions

---
+++ TaskManagementSystem.Core/Interfaces/ITaskRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Core.Interfaces
+{
+    public interface ITaskRepo
+    {
+        Task GetTaskById(int taskId);
+        IEnumerable<Task> GetAllTasks();
+        void CreateTask(Task task);
+        void UpdateTask(Task task);
+        void DeleteTask(int taskId);
+    }
+}
---
+++ TaskManagementSystem.Infrastructure/TaskRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementSystem.Core.Interfaces;
+
+namespace TaskManagementSystem.Infrastructure
+{
+    public class TaskRepo : ITaskRepo
+    {
+        public IEnumerable<Task> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetTaskById(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTask(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
